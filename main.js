2dc147e7366d4878b7dfe9299468af0980747ae6
erjon0 <erjonbenquku@gmail.com>
2024-10-24 16:16:52 +0200

    sup

---
+++ main.js
@@ -0,0 +1,15 @@
+var btn1 = document.getElementById("btn1")
+var texts = document.getElementById("texts")
+var h11 = document.getElementById("h11")
+var h12 = document.getElementById("h12")
+var number1 = document.getElementById('number1') 
+var number2 = document.getElementById('number2')
+var btn3 = document.getElementById('btn3')
+
+btn1.onclick{
+  texts.innerHTML + Input.value
+
+};
+                    
+
+
